26476fa534b9cdcb67ec2aaa9ce7ed6ea73b1e47
Stephen Port <por21022@byui.edu>
2022-06-16 14:46:36 -0500

    finished snake program

--- snake/game/scripting/move_actors_action.py
+++ snake/game/scripting/move_actors_action.py
@@ -1,4 +1,6 @@
 from game.scripting.action import Action
+from game.casting.actor import Actor
+from game.casting.cast import Cast
 
 
 # TODO: Implement MoveActorsAction class here! 
@@ -7,3 +9,19 @@ from game.scripting.action import Action
 # 1) get all the actors from the cast
 # 2) loop through the actors
 # 3) call the move_next() method on each actor
+
+class MoveActorsAction(Action):
+    """Override the execute method from the Action class"""
+
+    def __init__(self):
+        """Constructs a new MoveActorsAction"""
+        self.actor = Actor()
+        self.cast = Cast()
+        self.script = ""
+    
+    def execute(self, cast, script):
+        all_actors = self.cast.get_all_actors()
+        
+        for actor in all_actors:
+            actor.move_next()
+        
